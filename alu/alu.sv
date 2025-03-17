fdab10f0449e30a7b0cc2c0a6c414a4ff4786e2b
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2025-03-17 21:43:21 +0700

    Add files via upload

--- alu/alu.sv
+++ alu/alu.sv
@@ -1,13 +1,13 @@
 module alu (
-	input wire [31:0] a, b,
-	input wire [3:0] ALUControl,
-	output reg [63:0] result, 
-	output wire V, C, Z, N
+	input logic [31:0] a, b,
+	input logic [3:0] ALUControl,
+	output logic [63:0] result, 
+	output logic V, C, Z, N
 );
 
-wire [31:0] sum, sub, shifted;
-wire [63:0] mul;
-wire cout, gr, lt, eq;
+logic [31:0] sum, sub, shifted;
+logic [63:0] mul;
+logic cout, gr, lt, eq;
 
 fullAdder32b fa1 (.a(a), .b(b), .cin(ALUControl[0]), .sum(sum), .cout(cout));
 
@@ -20,14 +20,23 @@ comparator32b compare (.a(a), .b(b), .sign(ALUControl[2]), .Gr(gr), .Lt(lt), .Eq
 always @(*) begin
 	case (ALUControl)
 		4'b0000: begin
-						result [63:32] = 32'b0;
-						result [31:0] = sum;						// a + b
+						result [63:33] = 31'b0;
+						result [32] = cout;
+						result [31:0] = sum;					// a + b
 					end
 		4'b0001: begin
+				case (cout)
+					1'b0: begin
 						result [63:32] = 32'b0;
-						result [31:0] = sum;						// a - b
+						result [31:0] = sum;
+					end					// a - b
+					1'b1: begin
+						result [63:32] = 32'hffffffff;
+						result [31:0] = sum;
 					end
-		4'b0010:	result = mul;									// a * b
+				endcase
+			end
+		4'b0010: result = mul;									// a * b
 		4'b0011: begin
 						result [63:32] = 32'b0;
 						result [31:0] = a & b;					// and
