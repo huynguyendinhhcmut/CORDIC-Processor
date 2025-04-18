f8d7e8464f699af55b73f9efcd0533e858eeab17
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2025-04-18 09:29:33 +0700

    Add files via upload

--- alu/fpu.sv
+++ alu/fpu.sv
@@ -1,60 +1,51 @@
 module fpu (
-	input logic [31:0] A, B,
+	input logic [31:0] a, b,
 	input logic control,
 	output logic [31:0] result
 );
 
 logic cout, addsub;
-logic [7:0] d, dComp, shiftAmount, bigExponent;
-logic [31:0] smallShiftedOperand; 
+logic [7:0] largerExponent;
+logic [8:0] shiftAmount;
+logic [22:0] biggerOperand, smallerOperand;
 logic [32:0] sumOperand;
-logic [22:0] smallOperand, bigOperand;
+logic [31:0] shiftedOperand;
 
-signComputation sign (.signA(A[31]), .signB(B[31]), .cout(cout), .cin(control), .addsub(addsub), .resultSign(result[31]));
+signComputation sign (.signA(a[31]), .signB(b[31]), .cout(cout), .cin(control), .addsub(addsub), .resultSign(result[31]));
 
-fullAdder8b exponentDifference (.a(A[30:23]), .b(B[30:23]), .cin(1'b1), .cout(cout), .sum(d));
+exponentDifference ExponentDiffernce (.a(a[30:23]), .b(b[30:23]), .cout(cout), .shiftAmount(shiftAmount));
 
-fullAdder8b twoscomplement (.a(~d), .b(8'h1), .cin(1'b0), .sum(dComp));	
-	
 always @(*) begin
 	case (cout)
-		1'b0: shiftAmount = d;
-		1'b1: shiftAmount = dComp;
+		1'b0: biggerOperand = a[22:0];
+		1'b1: biggerOperand = b[22:0];
 	endcase
 end
 
 always @(*) begin
 	case (cout)
-		1'b0: bigOperand = A[22:0];
-		1'b1: bigOperand = B[22:0];
+		1'b0: smallerOperand = b[22:0];
+		1'b1: smallerOperand = a[22:0];
 	endcase
 end
+
+rightShifter RightShifter (.shiftAmount(shiftAmount), .smallerOperand(smallerOperand), .shiftedOperand(shiftedOperand));
+
+fullAdder32b fa32(.a({1'b1, biggerOperand, 8'b0}), .b(shiftedOperand), .cin(addsub), .sum(sumOperand[31:0]), .cout(sumOperand[32]));
 
 always @(*) begin
 	case (cout)
-		1'b0: smallOperand = B[22:0];
-		1'b1: smallOperand = A[22:0];
+		1'b0: largerExponent = a[30:23];
+		1'b1: largerExponent = b[30:23];
 	endcase
 end
 
-rightShifter rightshift(.shiftAmount(shiftAmount), .smallOperand(smallOperand), .shiftedOperand(smallShiftedOperand));
+normalized Normalized (.largerExponent(largerExponent), .sumOperand(sumOperand[32:1]), .resultExponent(result[30:23]), .resultOperand(result[22:0]));
 
-prefixAdder32b prefixAdder32b1 (.a({1'b1, bigOperand, 8'b0}), .b(smallShiftedOperand), .cin(addsub), .sum(sumOperand[31:0]), .cout(sumOperand[32]));
-	
-always @(*) begin
-		case (cout)
-			1'b0: bigExponent = A[30:23];
-			1'b1: bigExponent = B[30:23];
-		endcase
-end
-
-normalized normalized (.sumOperand(sumOperand[32:1]), .bigExponent(bigExponent), .resultOperand(result[22:0]), .resultExponent(result[30:23]));
-	
 endmodule
 
 
-
-
+//========== Sign Computation ==========
 module signComputation (
 	input logic signA, signB,
 	input logic cin, cout,
@@ -107,309 +98,195 @@ end
 
 endmodule
 
-
-
-
-module normalized ( 
+//========== Normalized ==========
+module normalized (
+	input logic [7:0] largerExponent,
 	input logic [31:0] sumOperand,
-	input logic [7:0] bigExponent,
-	output logic [22:0] resultOperand,
-	output logic [7:0] resultExponent
+	output logic [7:0] resultExponent,
+	output logic [22:0] resultOperand
 );
 
-logic [4:0] pre_addsubAmountExponent, addsubAmountExponent;
-logic addsub;
-logic [22:0] pre_resultOperand1, pre_resultOperand2, pre_resultOperand3;
+logic cin;
+logic [4:0] position, normalizedAmount;
 
-encoder32to5 encoder3251 (.a(sumOperand), .b(pre_addsubAmountExponent));
-
-mux32to1 addsubExp (.a(pre_addsubAmountExponent), .b(addsubAmountExponent), .addsub(addsub));
-
-fullAdder8b finalExponent (.a(bigExponent), .b({3'b000, addsubAmountExponent}), .cin(addsub), .sum(resultExponent));
+encoder32to5 encoder (.a(sumOperand), .b(position));
 
 always @(*) begin
-	case (addsubAmountExponent)
-		5'b0_0000: pre_resultOperand1 = sumOperand[29:7];
-		5'b0_0001: pre_resultOperand1 = sumOperand[28:6];
-		5'b0_0010: pre_resultOperand1 = sumOperand[27:5];
-		5'b0_0011: pre_resultOperand1 = sumOperand[26:4];
-		5'b0_0100: pre_resultOperand1 = sumOperand[25:3];
-		5'b0_0101: pre_resultOperand1 = sumOperand[24:2];
-		5'b0_0110: pre_resultOperand1 = sumOperand[23:1];
-		5'b0_0111: pre_resultOperand1 = sumOperand[22:0];
-		5'b0_1000: pre_resultOperand1 = {sumOperand[21:0], 1'b0};
-		5'b0_1001: pre_resultOperand1 = {sumOperand[20:0], 2'b0};
-		5'b0_1010: pre_resultOperand1 = {sumOperand[19:0], 3'b0};
-		5'b0_1011: pre_resultOperand1 = {sumOperand[18:0], 4'b0};
-		5'b0_1100: pre_resultOperand1 = {sumOperand[17:0], 5'b0};
-		5'b0_1101: pre_resultOperand1 = {sumOperand[16:0], 6'b0};
-		5'b0_1110: pre_resultOperand1 = {sumOperand[15:0], 7'b0};
-		5'b0_1111: pre_resultOperand1 = {sumOperand[14:0], 8'b0};
+	case (position)
+		5'b00000: begin 
+				cin = 1;
+				normalizedAmount = 5'b11110; //-30
+				resultOperand = 23'b0;
+			end
+		5'b00001: begin 
+				cin = 1;
+				normalizedAmount = 5'b11101; //-29
+				resultOperand = {sumOperand[0], 22'b0};
+			end
+		5'b00010: begin 
+				cin = 1;
+				normalizedAmount = 5'b11100; //-28
+				resultOperand = {sumOperand[1:0], 21'b0};
+			end
+		5'b00011: begin 
+				cin = 1;
+				normalizedAmount = 5'b11011; //-27
+				resultOperand = {sumOperand[2:0], 20'b0};
+			end
+		5'b00100: begin 
+				cin = 1;
+				normalizedAmount = 5'b11010; //-26
+				resultOperand = {sumOperand[3:0], 19'b0};
+			end
+		5'b00101: begin 
+				cin = 1;
+				normalizedAmount = 5'b11001; //-25
+				resultOperand = {sumOperand[4:0], 18'b0};
+			end
+		5'b00110: begin 
+				cin = 1;
+				normalizedAmount = 5'b11000; //-24
+				resultOperand = {sumOperand[5:0], 17'b0};
+			end
+		5'b00111: begin 
+				cin = 1;
+				normalizedAmount = 5'b10111; //-23
+				resultOperand = {sumOperand[6:0], 16'b0};
+			end
+		5'b01000: begin 
+				cin = 1;
+				normalizedAmount = 5'b10110; //-22
+				resultOperand = {sumOperand[7:0], 15'b0};
+			end
+		5'b01001: begin 
+				cin = 1;
+				normalizedAmount = 5'b10101; //-21
+				resultOperand = {sumOperand[8:0], 14'b0};
+			end
+		5'b01010: begin 
+				cin = 1;
+				normalizedAmount = 5'b10100; //-20
+				resultOperand = {sumOperand[9:0], 13'b0};
+			end
+		5'b01011: begin 
+				cin = 1;
+				normalizedAmount = 5'b10011; //-19
+				resultOperand = {sumOperand[10:0], 12'b0};
+			end
+		5'b01100: begin 
+				cin = 1;
+				normalizedAmount = 5'b10010; //-18
+				resultOperand = {sumOperand[11:0], 11'b0};
+			end
+		5'b01101: begin 
+				cin = 1;
+				normalizedAmount = 5'b10001; //-17
+				resultOperand = {sumOperand[12:0], 10'b0};
+			end
+		5'b01110: begin 
+				cin = 1;
+				normalizedAmount = 5'b10000; //-16
+				resultOperand = {sumOperand[13:0], 9'b0};
+			end
+		5'b01111: begin 
+				cin = 1;
+				normalizedAmount = 5'b01111; //-15
+				resultOperand = {sumOperand[14:0], 8'b0};
+			end
+		5'b10000: begin 
+				cin = 1;
+				normalizedAmount = 5'b01110; //-14
+				resultOperand = {sumOperand[15:0], 7'b0};
+			end
+		5'b10001: begin 
+				cin = 1;
+				normalizedAmount = 5'b01101; //-13
+				resultOperand = {sumOperand[16:0], 6'b0};
+			end
+		5'b10010: begin 
+				cin = 1;
+				normalizedAmount = 5'b01100; //-12
+				resultOperand = {sumOperand[17:0], 5'b0};
+			end
+		5'b10011: begin 
+				cin = 1;
+				normalizedAmount = 5'b01011; //-11
+				resultOperand = {sumOperand[18:0], 4'b0};
+			end
+		5'b10100: begin 
+				cin = 1;
+				normalizedAmount = 5'b01010; //-10
+				resultOperand = {sumOperand[19:0], 3'b0};
+			end
+		5'b10101: begin 
+				cin = 1;
+				normalizedAmount = 5'b01001; //-9
+				resultOperand = {sumOperand[20:0], 2'b0};
+			end
+		5'b10110: begin 
+				cin = 1;
+				normalizedAmount = 5'b01000; //-8
+				resultOperand = {sumOperand[21:0], 1'b0};
+			end
+		5'b10111: begin 
+				cin = 1;
+				normalizedAmount = 5'b00111; //-7
+				resultOperand = sumOperand[22:0];
+			end
+		5'b11000: begin 
+				cin = 1;
+				normalizedAmount = 5'b00110; //-6
+				resultOperand = sumOperand[23:1];
+			end
+		5'b11001: begin 
+				cin = 1;
+				normalizedAmount = 5'b00101; //-5
+				resultOperand = sumOperand[24:2];
+			end
 
-		5'b1_0000: pre_resultOperand1 = {sumOperand[13:0], 9'b0};
-		5'b1_0001: pre_resultOperand1 = {sumOperand[12:0], 10'b0};
-		5'b1_0010: pre_resultOperand1 = {sumOperand[11:0], 11'b0};
-		5'b1_0011: pre_resultOperand1 = {sumOperand[10:0], 12'b0};
-		5'b1_0100: pre_resultOperand1 = {sumOperand[9:0], 13'b0};
-		5'b1_0101: pre_resultOperand1 = {sumOperand[8:0], 14'b0};
-		5'b1_0110: pre_resultOperand1 = {sumOperand[7:0], 15'b0};
-		5'b1_0111: pre_resultOperand1 = {sumOperand[6:0], 16'b0};
-		5'b1_1000: pre_resultOperand1 = {sumOperand[5:0], 17'b0};
-		5'b1_1001: pre_resultOperand1 = {sumOperand[4:0], 18'b0};
-		5'b1_1010: pre_resultOperand1 = {sumOperand[3:0], 19'b0};
-		5'b1_1011: pre_resultOperand1 = {sumOperand[2:0], 20'b0};
-		5'b1_1100: pre_resultOperand1 = {sumOperand[1:0], 21'b0};
-		5'b1_1101: pre_resultOperand1 = {sumOperand[0], 22'b0};
-		5'b1_1110: pre_resultOperand1 = 23'b0;
-		5'b1_1111: pre_resultOperand1 = 23'b0;
+		5'b11010: begin 
+				cin = 1;
+				normalizedAmount = 5'b00100; //-4
+				resultOperand = sumOperand[25:3];
+			end
+
+		5'b11011: begin 
+				cin = 1;
+				normalizedAmount = 5'b00011; //-3
+				resultOperand = sumOperand[26:4];
+			end
+
+		5'b11100: begin 
+				cin = 1;
+				normalizedAmount = 5'b00010; //-2
+				resultOperand = sumOperand[27:5];
+			end
+
+		5'b11101: begin 
+				cin = 1;
+				normalizedAmount = 5'b00001; //-1
+				resultOperand = sumOperand[28:6];
+			end
+
+		5'b11110: begin 
+				cin = 0;
+				normalizedAmount = 5'b00000; //0
+				resultOperand = sumOperand[29:7];
+			end
+
+		5'b11111: begin 
+				cin = 0;
+				normalizedAmount = 5'b00001; //+1
+				resultOperand = sumOperand[30:8];
+			end
 	endcase
 end
 
-always @(*) begin
-	case (addsubAmountExponent)
-		5'b0_0000: pre_resultOperand2 = sumOperand[29:7];
-		5'b0_0001: pre_resultOperand2 = sumOperand[30:8];
-		default: pre_resultOperand2 = 22'b0;
-	endcase
-end
+fullAdder8b fa8 (.a(largerExponent), .b({3'b000, normalizedAmount}), .cin(cin), .sum(resultExponent));
 
-always @(*) begin
-	case (addsub)
-		1'b0: pre_resultOperand3 = pre_resultOperand2;
-		1'b1: pre_resultOperand3 = pre_resultOperand1;
-	endcase
-end
-		
-always @(*) begin
-	case (&resultExponent)
-		1'b0: resultOperand = pre_resultOperand3;
-		1'b1: resultOperand = 22'b0;
-	endcase
-end
+endmodule	
 
-endmodule
-
-
-
-
-module mux32to1 (
-	input logic [4:0] a,
-	output logic [4:0] b,
-	output logic addsub
-);
-
-logic [4:0] b0, b1, b2, b3;
-logic addsub0, addsub1, addsub2, addsub3;
-
-always @(*) begin
-		case (a[2:0])
-			3'b000: begin
-					b0 = 5'b1_1110;	//30
-					addsub0 = 1'b1;
-			end
-			3'b001: begin
-					b0 = 5'b1_1101;	//29
-					addsub0 = 1'b1;
-			end
-			
-			3'b010: begin
-					b0 = 5'b1_1100;	//28
-					addsub0 = 1'b1;
-			end
-			
-			3'b011: begin
-					b0 = 5'b1_1011;	//27
-					addsub0 = 1'b1;
-			end
-
-			3'b100: begin
-					b0 = 5'b1_1010;	//26
-					addsub0 = 1'b1;
-			end
-
-			3'b101: begin
-					b0 = 5'b1_1001;	//25
-					addsub0 = 1'b1;
-			end
-
-			3'b110: begin
-					b0 = 5'b1_1000;	//24
-					addsub0 = 1'b1;
-			end
-
-			3'b111: begin
-					b0 = 5'b1_0111;	//23
-					addsub0 = 1'b1;
-			end
-
-		endcase
-
-end
-
-always @(*) begin
-		case (a[2:0])
-			3'b000: begin
-					b1 = 5'b1_0110;	//22
-					addsub1 = 1'b1;
-			end
-			3'b001: begin
-					b1 = 5'b1_0101;	//21
-					addsub1 = 1'b1;
-			end
-			
-			3'b010: begin
-					b1 = 5'b1_0100;	//20
-					addsub1 = 1'b1;
-			end
-			
-			3'b011: begin
-					b1 = 5'b1_0011;	//19
-					addsub1 = 1'b1;
-			end
-
-			3'b100: begin
-					b1 = 5'b1_0010;	//18
-					addsub1 = 1'b1;
-			end
-
-			3'b101: begin
-					b1 = 5'b1_0001;	//17
-					addsub1 = 1'b1;
-			end
-
-			3'b110: begin
-					b1 = 5'b1_0000;	//16
-					addsub1 = 1'b1;
-			end
-
-			3'b111: begin
-					b1 = 5'b0_1111;	//15
-					addsub1 = 1'b1;
-			end
-			
-		endcase
-end
-
-always @(*) begin
-		case (a[2:0])
-			3'b000: begin
-					b2 = 5'b0_1110;	//14
-					addsub2 = 1'b1;
-			end
-			3'b001: begin
-					b2 = 5'b0_1101;	//13
-					addsub2 = 1'b1;
-			end
-			
-			3'b010: begin
-					b2 = 5'b0_1100;	//12
-					addsub2 = 1'b1;
-			end
-			
-			3'b011: begin
-					b2 = 5'b0_1011;	//11
-					addsub2 = 1'b1;
-			end
-
-			3'b100: begin
-					b2 = 5'b0_1010;	//10
-					addsub2 = 1'b1;
-			end
-
-			3'b101: begin
-					b2 = 5'b0_1001;	//9
-					addsub2 = 1'b1;
-			end
-
-			3'b110: begin
-					b2 = 5'b0_1000;	//8
-					addsub2 = 1'b1;
-			end
-
-			3'b111: begin
-					b2 = 5'b0_0111;	//7
-					addsub2 = 1'b1;
-			end
-
-		endcase
-
-end
-
-always @(*) begin
-		case (a[2:0])
-			3'b000: begin
-					b3 = 5'b0_0110;	//6
-					addsub3 = 1'b1;
-			end
-			3'b001: begin
-					b3 = 5'b0_0101;	//5
-					addsub3 = 1'b1;
-			end
-			
-			3'b010: begin
-					b3 = 5'b0_0100;	//4
-					addsub3 = 1'b1;
-			end
-			
-			3'b011: begin
-					b3 = 5'b0_0011;	//3
-					addsub3 = 1'b1;
-			end
-
-			3'b100: begin
-					b3 = 5'b0_0010;	//2
-					addsub3 = 1'b1;
-			end
-
-			3'b101: begin
-					b3 = 5'b0_0001;	//1
-					addsub3 = 1'b1;
-			end
-
-			3'b110: begin
-					b3 = 5'b0_0000;	//0
-					addsub3 = 1'b0;
-			end
-			
-			3'b111: begin
-					b3 = 5'b0_0001;	//1
-					addsub3 = 1'b0;
-			end
-
-		endcase
-
-end
-
-always @(*) begin
-		case (a[4:3])
-			2'b00: begin
-					b = b0;	
-					addsub = addsub0;
-			end
-			2'b01: begin
-					b = b1;	
-					addsub = addsub1;
-			end
-			
-			2'b10: begin
-					b = b2;	
-					addsub = addsub2;
-			end
-			
-			2'b11: begin
-					b = b3; 
-					addsub = addsub3;
-			end
-			
-		endcase
-
-end
-
-endmodule
-
-
-
-
+//========== Encoder 32 to 5 ==========
 module encoder32to5 (
 	input logic [31:0] a,
 	output logic [4:0] b
@@ -447,9 +324,6 @@ end
 
 endmodule
 
-
-
-
 module encoder8to3 (
 	input logic [7:0] a,
 	output logic [2:0] b
@@ -460,9 +334,6 @@ assign b[1] = a[7] | a[6] | ((~a[5]) & (~a[4]) & a[3]) | ((~a[5]) & (~a[4]) & a[
 assign b[0] = a[7] | ((~a[6]) & a[5]) | ((~a[6]) & (~a[4]) & a[3]) | ((~a[6]) & (~a[4]) & (~a[2]) & a[1]);
 
 endmodule
-
-
-
 
 module encoder4to2 (
 	input logic [3:0] a,
@@ -475,20 +346,19 @@ assign b[0] = (a[1] | a[3]) & ((~a[2]) | a[3]);
 endmodule
 
 
-
-
+//========== Right Shifter ==========
 module rightShifter (
-	input logic [7:0] shiftAmount,
-	input logic [22:0] smallOperand,
+	input logic [8:0] shiftAmount,
+	input logic [22:0] smallerOperand,
 	output logic [31:0] shiftedOperand
 );
 
-logic [31:0] shift1, shift2, shift3, shift4, shift5, shift6, shift7;
+logic [31:0] shift1, shift2, shift3, shift4, shift5, shift6, shift7, shift8;
 
 always @(*) begin 
 	case (shiftAmount[0])
-		1'b0: shift1 = {1'b1, smallOperand, 8'b0};
-		1'b1: shift1 = {1'b0, 1'b1, smallOperand, 7'b0};
+		1'b0: shift1 = {1'b1, smallerOperand, 8'b0};
+		1'b1: shift1 = {1'b0, 1'b1, smallerOperand, 7'b0};
 		default: shift1 = 32'b0;
 	endcase
 end
@@ -543,7 +413,15 @@ end
 
 always @(*) begin 
 	case (shiftAmount[7])
-		1'b0: shiftedOperand = shift7;
+		1'b0: shift8 = shift7;
+		1'b1: shift8 = 32'b0;
+		default: shift8 = 32'b0;
+	endcase
+end
+
+always @(*) begin 
+	case (shiftAmount[8])
+		1'b0: shiftedOperand = shift8;
 		1'b1: shiftedOperand = 32'b0;
 		default: shiftedOperand = 32'b0;
 	endcase
@@ -551,12 +429,58 @@ end
 
 endmodule
 
+//========== Exponent Difference ==========
+module exponentDifference (
+	input logic [7:0] a, b,
+	output logic cout,
+	output logic [8:0] shiftAmount
+);
+
+logic [8:0] d;		// d = Ea - Eb
+logic [9:0] d_2c;	// 2's complement d
+
+assign cout = d[8];
+
+fullAdder8b fa8 (.a(a), .b(b), .cin(1'b1), .sum(d[7:0]), .cout(d[8]));
+fullAdder9b fa9 (.a(~d), .b(9'b000000001), .cin(1'b0), .sum(d_2c[8:0]), .cout(d_2c[9]));
+
+always @(*) begin
+	case (cout)
+		1'b0: shiftAmount = d;
+		1'b1: shiftAmount = d_2c[8:0];
+	endcase
+end
+
+endmodule
 
 
+//========== Full Adder ==========
+module fullAdder9b (
+        input logic [8:0] a, b,
+	input logic cin,
+        output logic [8:0] sum,
+        output logic cout
+);
+
+logic [8:0] carry;
+
+fullAdder fa9 (.sum(sum[0]),   .a(a[0]),  .b(b[0]^cin),  .cin(cin),       .cout(carry[0]));
+fullAdder fa10 (.sum(sum[1]),   .a(a[1]),  .b(b[1]^cin),  .cin(carry[0]),  .cout(carry[1]));
+fullAdder fa11 (.sum(sum[2]),   .a(a[2]),  .b(b[2]^cin),  .cin(carry[1]),  .cout(carry[2]));
+fullAdder fa12 (.sum(sum[3]),   .a(a[3]),  .b(b[3]^cin),  .cin(carry[2]),  .cout(carry[3]));
+fullAdder fa13 (.sum(sum[4]),   .a(a[4]),  .b(b[4]^cin),  .cin(carry[3]),  .cout(carry[4]));
+fullAdder fa14 (.sum(sum[5]),   .a(a[5]),  .b(b[5]^cin),  .cin(carry[4]),  .cout(carry[5]));
+fullAdder fa15 (.sum(sum[6]),   .a(a[6]),  .b(b[6]^cin),  .cin(carry[5]),  .cout(carry[6]));
+fullAdder fa16 (.sum(sum[7]),   .a(a[7]),  .b(b[7]^cin),  .cin(carry[6]),  .cout(carry[7]));
+fullAdder fa17 (.sum(sum[8]),   .a(a[8]),  .b(b[8]^cin),  .cin(carry[7]),  .cout(carry[8]));
+
+xor (cout, carry[8], cin);
+
+endmodule
 
 module fullAdder8b (
         input logic [7:0] a, b,
-		  input logic cin,
+	input logic cin,
         output logic [7:0] sum,
         output logic cout
 );
@@ -575,4 +499,3 @@ fullAdder fa8 (.sum(sum[7]),   .a(a[7]),  .b(b[7]^cin),  .cin(carry[6]),  .cout(
 xor (cout, carry[7], cin);
 
 endmodule
-
