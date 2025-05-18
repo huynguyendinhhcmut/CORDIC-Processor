2e5402d8436628fa60d8051dd996c1e60c4e5261
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2025-05-18 18:57:11 +0700

    Add files via upload

--- dv/cordic_tb.sv
+++ dv/cordic_tb.sv
@@ -39,29 +39,28 @@ module cordic_tb;
     reg [31:0] test_angles [0:20];  // You can add more angles here
     begin
       	// Floating point 32-bit angles in radians
-      	test_angles[0] = 32'h00000000;  // 0
-      	test_angles[1] = 32'h3ea0d97c;  // 18
-      	test_angles[2] = 32'h3f20d97c;  // 36
-      	test_angles[3] = 32'h3f71463a;  // 54
-      	test_angles[4] = 32'h3fa0d97c;  // 72
-      	test_angles[5] = 32'h3fc90fdb;  // 90
-      	test_angles[6] = 32'h3ff1463a;  // 108
-      	test_angles[7] = 32'h400cbe4c;  // 126
-      	test_angles[8] = 32'h4020d97c;  // 144
-      	test_angles[9] = 32'h4034f4ab;  // 162
-      	test_angles[10] = 32'h40490fdb; // 180 
+      	test_angles[0] = 32'h3e685696;  // 13
+      	test_angles[1] = 32'h3efa35dd;  // 28
+      	test_angles[2] = 32'h3f490fdb;  // 45
+      	test_angles[3] = 32'h3f95adf0;  // 67
+      	test_angles[4] = 32'h3fbba866;  // 84
+      	test_angles[5] = 32'h3fd8b338;  // 97
+      	test_angles[6] = 32'h3ffc71c6;  // 113
+      	test_angles[7] = 32'h40101829;  // 129
+      	test_angles[8] = 32'h4028ab2b;  // 151
+      	test_angles[9] = 32'h40425c20;  // 174
+      	test_angles[10] = 32'h404c69b8; // 183 
       	test_angles[11] = 32'h405d2b0a; // 198
-      	test_angles[12] = 32'h4071463a; // 216
-      	test_angles[13] = 32'h4082b0b5; // 234
-      	test_angles[14] = 32'h408cbe4c; // 252
-      	test_angles[15] = 32'h4096cbe4; // 270
-      	test_angles[16] = 32'h40a0d97c; // 288
-      	test_angles[17] = 32'h40aae713; // 306
-      	test_angles[18] = 32'h40b4f4ab; // 324
-      	test_angles[19] = 32'h40bf0243; // 342
-      	test_angles[20] = 32'h40c90fdb; // 360
+      	test_angles[12] = 32'h406bb073; // 211
+      	test_angles[13] = 32'h40833faf; // 235
+      	test_angles[14] = 32'h4095adf0; // 268
+      	test_angles[15] = 32'h409ab4bb; // 277
+      	test_angles[16] = 32'h40a2866a; // 291
+      	test_angles[17] = 32'h40afeddf; // 315
+      	test_angles[18] = 32'h40bcc65a; // 338
+      	test_angles[19] = 32'h40c880e0; // 359
 
-      for (i = 0; i < 21; i = i + 1) begin
+      for (i = 0; i < 20; i = i + 1) begin
         start = 0;
         #4;
 	angle = test_angles[i];
