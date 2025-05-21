5d0eb45bee4d2ba51d5d1489415b28b9d2d24569
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2025-05-21 21:06:33 +0700

    Update cordic_processor.sv

--- rtl/cordic_processor.sv
+++ rtl/cordic_processor.sv
@@ -7,6 +7,6 @@ logic [31:0] DIN, DOUT, ADDR;
 
 processor processor (.clk(clk), .rst_n(rst_n), .run(run), .DIN(DIN), .ADDR(ADDR), .DOUT(DOUT), .W(W));
 
-ram_handmade ram (.clk(clk), .wr_en(W), .address(ADDR), .data_in(DOUT), .data_out(DIN));
+ram_handmade ram (.clk(clk), .wr_en(W), .address(ADDR[9:0]), .data_in(DOUT), .data_out(DIN));
 
 endmodule
