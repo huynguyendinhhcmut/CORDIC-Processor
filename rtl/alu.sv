d4477fad438cabe9e095b3ba9435b68ead9410d2
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2025-10-23 12:10:35 +0700

    Add files via upload

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,51 +1,32 @@
 module alu (
-	input logic [31:0] a, b,
-	input logic [3:0] ALUControl,
-	output logic [31:0] result,
-	output logic V
+	input logic [31:0] i_operand_a,	// First operand for ALU operations.
+	input logic [31:0] i_operand_b,	// Second operand for ALU operations.
+	input logic [3:0] i_alu_op,		// The operation to be performed.
+	
+	output logic [31:0] o_alu_data	// Result of the ALU operation.
 );
 
-logic [31:0] sum, sub, shifted, fpu;
-logic cout, gr, lt, eq;
+logic [31:0] sum, shifted;
+logic less;
 
-prefixAdder32b pa1 (.a(a), .b(b^{32{ALUControl[0]}}), .cin(ALUControl[0]), .sum(sum), .cout(V));
+fullAdder32b fa1 (.a(i_operand_a), .b(i_operand_b), .cin(i_alu_op[0]), .sum(sum));
+comparator32b_signed comp_signed (.a(i_operand_a), .b(i_operand_b), .signed_mode(~i_alu_op[0]), .Lt(less));
+shift shift (.data_in(i_operand_a), .rightleft(i_alu_op[2]), .arith(i_alu_op[0] & i_alu_op[3]), .shift_amount(i_operand_b[4:0]), .data_out(shifted));
 
-shift shift1 (.data_in(a), .shift_amount(b[4:0]), .rightleft(ALUControl[0]), .arith(ALUControl[3]), .data_out(shifted));
-
-comparator32b compare (.a(a), .b(b), .Gr(gr), .Lt(lt), .Eq(eq));
-
-fpu fpu1 (.a(a), .b(b), .control(~ALUControl[0]), .mul(ALUControl[1] & ALUControl[0]), .result(fpu));
-
-always @(*) begin
-	case (ALUControl)
-		4'b0000: result [31:0] = sum;						// a + b
-		4'b0001: result [31:0] = sum;						// a - b						
-		4'b0010: result [31:0] = a & b;					// and	
-		4'b0011: result [31:0] = a | b;					// or	
-		4'b0100: result [31:0] = a ^ b; 					// xor	
-		4'b0101: result [31:0] = ~a;						// not 
-		4'b0110: result [31:0] = shifted;				// right shift logic	
-		4'b0111: result [31:0] = shifted;				// left shift logic	
-		4'b1000: result [31:0] = shifted; 				// right shift arithmetic
-		4'b1001: case (eq) 
-						1'b0: result = 32'b0;
-						1'b1: result = 32'b1;				// check equal
-					endcase
-		4'b1010: case (lt) 
-						1'b0: result = 32'b0;
-						1'b1: result = 32'b1;				// check less
-					endcase
-		4'b1011: case (gr | eq) 
-						1'b0: result = 32'b0;
-						1'b1: result = 32'b1;				// check greater or equal
-					endcase
-		4'b1100: case (~eq) 
-						1'b0: result = 32'b0;
-						1'b1: result = 32'b1;				// check not equal				
-					endcase				
-		4'b1101: result [31:0] = fpu;						// add floating point		
-		4'b1110: result [31:0] = fpu;						// sub floating point		
-		4'b1111: result [31:0] = fpu;						// mul floating point
+always_comb begin
+	case (i_alu_op)
+		4'b0000: o_alu_data = sum;									// ADD
+		4'b0001:	o_alu_data = sum;									// SUB
+		4'b0010: o_alu_data = {31'b0, less};					// SLT
+		4'b0011: o_alu_data = {31'b0, less};					// SLTU
+		4'b0100: o_alu_data = i_operand_a ^ i_operand_b;	// XOR
+		4'b0101: o_alu_data = i_operand_a | i_operand_b;	// OR
+		4'b0110: o_alu_data = i_operand_a & i_operand_b;	// AND
+		4'b0111: o_alu_data = shifted;							// SLL
+		4'b1000: o_alu_data = shifted;							// SRL
+		4'b1001: o_alu_data = shifted;							// SRA
+		4'b1010: o_alu_data = i_operand_b;						// LUI
+		default: o_alu_data = 32'b0;
 	endcase
 end
 
